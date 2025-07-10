3b31ff3bac0f655c59da9fa6becc8f11a9900238
Anthony Achibi <anthonyachibi@gmail.com>
2025-07-10 15:48:47 +0100

    '-'

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductCard from '../components/ProductCard'
+function Products() {
+  return (
+    <div className="">
+      <ProductCard />
+    </div>
+  )
+}
+
+export default Products
